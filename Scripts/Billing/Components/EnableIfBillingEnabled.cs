cbd56d92584ceaeebb72b7d4ac7eb0c2b4d5b6c7
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2016-01-29 12:40:41 +0100

    Component to Hide / Show gameobject based upon whether IAP is enabled

---
+++ Scripts/Billing/Components/EnableIfBillingEnabled.cs
@@ -0,0 +1,24 @@
+﻿//----------------------------------------------
+// Flip Web Apps: Game Framework
+// Copyright © 2016 Flip Web Apps / Mark Hewitt
+//----------------------------------------------
+
+using FlipWebApps.GameFramework.Scripts.GameObjects.Components;
+
+namespace FlipWebApps.GameFramework.Scripts.Billing.Components
+{
+    /// <summary>
+    /// Enabled or a disabled a gameobject based upon whether the facebook SDK is installed
+    /// </summary>
+    public class EnableIfBillingEnabled : EnableDisableGameObject
+    {
+        public override bool IsConditionMet()
+        {
+#if UNITY_PURCHASING
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
