acb6eaecfd24d31a37b998b0d3fa4491764191be
unknown <mark_a_hewitt@yahoo.co.uk>
2016-02-09 11:28:20 +0100

    Generic run once base component Updated animation SetTriggerOnce to use generic base Added animation SetBoolOnce

--- Scripts/Animation/Components/SetBoolOnce.cs
+++ Scripts/Animation/Components/SetBoolOnce.cs
@@ -9,16 +9,17 @@ using UnityEngine;
 namespace FlipWebApps.GameFramework.Scripts.Animation.Components
 {
     /// <summary>
-    /// Set an animation trigger only one time and optionally after another animation has already been triggered
+    /// Set an animation bool only one time and optionally after another animation has already been triggered
     /// </summary>
-    public class SetTriggerOnce : RunOnceGameObject
+    public class SetBoolOnce : RunOnceGameObject
     {
         public Animator Animator;
-        public string Trigger;
+        public string Name;
+        public bool Value;
 
         public override void RunOnce()
         {
-            Animator.SetTrigger(Trigger);
+            Animator.SetBool(Name, Value);
         }
     }
 }
---
+++ Scripts/GameObjects/Components/EnableOnceGameObject.cs
@@ -0,0 +1,37 @@
+﻿//----------------------------------------------
+// Flip Web Apps: Game Framework
+// Copyright © 2016 Flip Web Apps / Mark Hewitt
+//----------------------------------------------
+
+using UnityEngine;
+
+namespace FlipWebApps.GameFramework.Scripts.GameObjects.Components
+{
+    /// <summary>
+    /// An abstract class to show run something one time only.
+    /// 
+    /// Override and implement the condition as you best see fit.
+    /// </summary>
+    public abstract class RunOnceGameObject : RunOnState
+    {
+        public string Key;
+        public string EnableAfterKey;
+
+        public override void RunMethod()
+        {
+            // show hint panel first time only
+            if (string.IsNullOrEmpty(EnableAfterKey) || PlayerPrefs.GetInt("AnimationTriggerOnce." + EnableAfterKey, 0) == 1)
+            {
+                if (PlayerPrefs.GetInt("AnimationTriggerOnce." + Key, 0) == 0)
+                {
+                    PlayerPrefs.SetInt("AnimationTriggerOnce." + Key, 1);
+                    PlayerPrefs.Save();
+
+                    RunOnce();
+                }
+            }
+        }
+
+        public abstract void RunOnce();
+    }
+}
