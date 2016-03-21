518898a98043ad2a6f783663c2caf8711163e7d8
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2016-03-21 11:59:10 +0100

    Added option to specify top and bottom y position of the gradient

--- Scripts/Display/Other/Components/GradientBackground.cs
+++ Scripts/Display/Other/Components/GradientBackground.cs
@@ -37,6 +37,10 @@ namespace FlipWebApps.GameFramework.Scripts.Display.Other.Components
         public Color BottomColor = Color.white;
         public Shader Shader;
         public int GradientLayer = 7;
+        [Range(0, 1)]
+        public float NormalisedTop = 1;
+        [Range(0, 1)]
+        public float NormalisedBottom = 0;
 
         void Awake()
         {
@@ -54,13 +58,16 @@ namespace FlipWebApps.GameFramework.Scripts.Display.Other.Components
             gradientCam.depth = attachedCamera.depth - 1;
             gradientCam.cullingMask = 1 << GradientLayer;
 
+            //1.154 is the height of a plane at z=0 with the default camera field of view of 60. .577 is half this.
+            var top = -.577f + (NormalisedTop*1.154f);
+            var bottom = -.577f + (NormalisedBottom * 1.154f);
             var mesh = new Mesh
             {
                 vertices =
                     new[]
                     {
-                        new Vector3(-100f, .577f, 1f), new Vector3(100f, .577f, 1f), new Vector3(-100f, -.577f, 1f),
-                        new Vector3(100f, -.577f, 1f)
+                        new Vector3(-100f, top, 1f), new Vector3(100f, top, 1f), new Vector3(-100f, bottom, 1f),
+                        new Vector3(100f, bottom, 1f)
                     },
                 colors = new[] {TopColor, TopColor, BottomColor, BottomColor},
                 triangles = new[] {0, 1, 2, 1, 3, 2}
@@ -78,56 +85,4 @@ namespace FlipWebApps.GameFramework.Scripts.Display.Other.Components
             gradientPlane.layer = GradientLayer;
         }
     }
-
-    //}
-
-
-    //using UnityEngine;
-
-    ///// <summary>
-    ///// Description
-    ///// Creates a background for the camera, which is a simple gradient blend between two colors.
-    ///// Usage
-    ///// Attach this script to your camera, and change the top and bottom colors in the inspector as desired. When run, the clear flags for your camera is set to Depth Only, allowing a newly created background camera to show through. A plane with the gradient colors is created, which only the background camera can see. This is done with GradientLayer, which is the only layer that the background camera sees, and your camera is set to ignore. The default, 7, is a built-in (non-user-editable) layer that's not used for anything as of Unity 3.1. It can be changed as necessary.
-    ///// </summary>
-    //public class GradientBackground : MonoBehaviour
-    //{
-    //    public Camera camera;
-    //    public Gradient gradient;
-
-    //    public Color topColor = Color.blue;
-    //    public Color bottomColor = Color.white;
-    //    public int GradientLayer = 7;
-    //    public float bottomPosition = -.577f;
-    //    void Awake()
-    //    {
-    //        GradientLayer = Mathf.Clamp(GradientLayer, 0, 31);
-    //        if (!GetComponent<Camera>())
-    //        {
-    //            Debug.LogError("Must attach GradientBackground script to the camera");
-    //            return;
-    //        }
-
-    //        GetComponent<Camera>().clearFlags = CameraClearFlags.Depth;
-    //        GetComponent<Camera>().cullingMask = GetComponent<Camera>().cullingMask & ~(1 << GradientLayer);
-    //        Camera gradientCam = new GameObject("Gradient Cam", typeof(Camera)).GetComponent<Camera>();
-    //        gradientCam.depth = GetComponent<Camera>().depth - 1;
-    //        gradientCam.cullingMask = 1 << GradientLayer;
-
-    //        Mesh mesh = new Mesh();
-    //        mesh.vertices = new Vector3[4] { new Vector3(-100f, .577f, 1f), new Vector3(100f, .577f, 1f), new Vector3(-100f, bottomPosition, 1f), new Vector3(100f, bottomPosition, 1f) };
-
-    //        mesh.colors = new Color[4] { topColor, topColor, bottomColor, bottomColor };
-
-    //        mesh.triangles = new int[6] { 0, 1, 2, 1, 3, 2 };
-
-    //        Material mat = new Material("Shader \"Vertex Color Only\"{Subshader{BindChannels{Bind \"vertex\", vertex Bind \"color\", color}Pass{}}}");
-    //        GameObject gradientPlane = new GameObject("Gradient Plane", typeof(MeshFilter), typeof(MeshRenderer));
-
-    //        ((MeshFilter)gradientPlane.GetComponent(typeof(MeshFilter))).mesh = mesh;
-    //        gradientPlane.GetComponent<Renderer>().material = mat;
-    //        gradientPlane.layer = GradientLayer;
-    //    }
-
-    //}
 }
