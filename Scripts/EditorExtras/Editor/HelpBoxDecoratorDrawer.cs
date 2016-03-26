00237188b809c92064d011fa6e83042e0e961951
unknown <mhew@LT-889943.statoil.net>
2016-03-26 15:26:17 +0100

    Added HelpBox Decorator drawer

---
+++ Scripts/EditorExtras/Editor/HelpBoxDecoratorDrawer.cs
@@ -0,0 +1,45 @@
+﻿//----------------------------------------------
+// Flip Web Apps: Game Framework
+// Copyright © 2016 Flip Web Apps / Mark Hewitt
+//
+// Please direct any bugs/comments/suggestions to http://www.flipwebapps.com
+// 
+// The copyright owner grants to the end user a non-exclusive, worldwide, and perpetual license to this Asset
+// to integrate only as incorporated and embedded components of electronic games and interactive media and 
+// distribute such electronic game and interactive media. End user may modify Assets. End user may otherwise 
+// not reproduce, distribute, sublicense, rent, lease or lend the Assets. It is emphasized that the end 
+// user shall not be entitled to distribute or transfer in any way (including, without, limitation by way of 
+// sublicense) the Assets in any other way than as integrated components of electronic games and interactive media. 
+
+// The above copyright notice and this permission notice must not be removed from any files.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//----------------------------------------------
+
+using UnityEngine;
+using UnityEditor;
+
+namespace FlipWebApps.GameFramework.Scripts.EditorExtras.Editor
+{
+    /// <summary>
+    /// Decorator drawer for showing a help box with text and type specified by HelpBoxAttribute
+    /// </summary>
+    [CustomPropertyDrawer(typeof(HelpBoxAttribute))]
+    public class HelpBoxDecoratorDrawer : DecoratorDrawer
+    {
+        public override void OnGUI(Rect position)
+        {
+
+           var helpBoxAttribute = (HelpBoxAttribute)attribute;
+            EditorGUI.HelpBox(position, helpBoxAttribute.Text, helpBoxAttribute.Type);
+        }
+
+        public override float GetHeight()
+        {
+            return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+}
