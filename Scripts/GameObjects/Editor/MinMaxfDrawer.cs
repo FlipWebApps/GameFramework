08c7a9c4246ac59f21109e751c0c81ae90cd62c9
unknown <mhew@LT-889943.statoil.net>
2016-03-26 15:28:05 +0100

    Custom MinMax property drawer

---
+++ Scripts/GameObjects/Editor/MinMaxfDrawer.cs
@@ -0,0 +1,45 @@
+﻿//----------------------------------------------
+// Flip Web Apps: Game Framework
+// Copyright © 2016 Flip Web Apps / Mark Hewitt
+//
+// Please direct any bugs/comments/suggestions to http://www.flipwebapps.com
+// 
+// The copyright owner grants to the end user a non-exclusive, worldwide, and perpetual license to this Asset
+// to integrate only as incorporated and embedded components of electronic games and interactive media and 
+// distribute such electronic game and interactive media. End user may modify Assets. End user may otherwise 
+// not reproduce, distribute, sublicense, rent, lease or lend the Assets. It is emphasized that the end 
+// user shall not be entitled to distribute or transfer in any way (including, without, limitation by way of 
+// sublicense) the Assets in any other way than as integrated components of electronic games and interactive media. 
+
+// The above copyright notice and this permission notice must not be removed from any files.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//----------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using FlipWebApps.GameFramework.Scripts.GameObjects;
+
+namespace FlipWebApps.GameFramework.Scripts.GameObjects.Editor
+{
+    [CustomPropertyDrawer(typeof(MinMaxf))]
+    public class MinMaxfDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            label = EditorGUI.BeginProperty(position, label, property);
+            Rect contentPosition = EditorGUI.PrefixLabel(position, label);
+            contentPosition.width /= 2;
+            EditorGUIUtility.labelWidth = 30;
+            EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("Min"), new GUIContent("Min"));
+            contentPosition.x += contentPosition.width;
+            EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("Max"));
+            EditorGUI.EndProperty();
+        }
+
+    }
+}
