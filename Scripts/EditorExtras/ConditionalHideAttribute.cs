c703cb339788478b2b44528f9fbcf92c384b9f14
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2016-04-23 19:45:08 +0200

    EditorExtras: Added namespace to ConditionalHideAttribute and Property Drawer.

--- Scripts/EditorExtras/ConditionalHideAttribute.cs
+++ Scripts/EditorExtras/ConditionalHideAttribute.cs
@@ -1,40 +1,42 @@
-﻿using UnityEngine;
-using System;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
 //Original version of the ConditionalHideAttribute created by Brecht Lecluyse (www.brechtos.com)
 //Modified by: -
 
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
-public class ConditionalHideAttribute : PropertyAttribute
+namespace FlipWebApps.GameFramework.Scripts.EditorExtras
 {
-    public string ConditionalSourceField = "";
-    public string ConditionalSourceField2 = "";
-    public bool HideInInspector = false;
-    public bool Inverse = false;
-
-	// Use this for initialization
-    public ConditionalHideAttribute(string conditionalSourceField)
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+    public class ConditionalHideAttribute : PropertyAttribute
     {
-        this.ConditionalSourceField = conditionalSourceField;
-        this.HideInInspector = false;
-        this.Inverse = false;
-    }
+        public string ConditionalSourceField = "";
+        public string ConditionalSourceField2 = "";
+        public bool HideInInspector = false;
+        public bool Inverse = false;
 
-    public ConditionalHideAttribute(string conditionalSourceField, bool hideInInspector)
-    {
-        this.ConditionalSourceField = conditionalSourceField;
-        this.HideInInspector = hideInInspector;
-        this.Inverse = false;
-    }
+        // Use this for initialization
+        public ConditionalHideAttribute(string conditionalSourceField)
+        {
+            this.ConditionalSourceField = conditionalSourceField;
+            this.HideInInspector = false;
+            this.Inverse = false;
+        }
 
-    public ConditionalHideAttribute(string conditionalSourceField, bool hideInInspector, bool inverse)
-    {
-        this.ConditionalSourceField = conditionalSourceField;
-        this.HideInInspector = hideInInspector;
-        this.Inverse = inverse;
-    }
+        public ConditionalHideAttribute(string conditionalSourceField, bool hideInInspector)
+        {
+            this.ConditionalSourceField = conditionalSourceField;
+            this.HideInInspector = hideInInspector;
+            this.Inverse = false;
+        }
+
+        public ConditionalHideAttribute(string conditionalSourceField, bool hideInInspector, bool inverse)
+        {
+            this.ConditionalSourceField = conditionalSourceField;
+            this.HideInInspector = hideInInspector;
+            this.Inverse = inverse;
+        }
    
+    }
 }
 
 
