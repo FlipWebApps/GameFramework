d7a21bc2dbfe455ad8b7eaf6f118ae32665b06d4
unknown <mhew@LT-889943.statoil.net>
2016-04-23 20:10:13 +0200

    EditorExtras: Added EditorHelper class with useful Editor related helper functions

---
+++ Scripts/EditorExtras/Editor/EditorHelper.cs
@@ -0,0 +1,65 @@
+﻿//----------------------------------------------
+// Flip Web Apps: Game Framework
+// Copyright © 2016 Flip Web Apps / Mark Hewitt
+//
+// Please direct any bugs/comments/suggestions to http://www.flipwebapps.com
+// 
+// The copyright owner grants to the end user a non-exclusive, worldwide, and perpetual license to this Asset
+// to integrate only as incorporated and embedded components of electronic games and interactive media and 
+// distribute such electronic game and interactive media. End user may modify Assets. End user may otherwise 
+// not reproduce, distribute, sublicense, rent, lease or lend the Assets. It is emphasized that the end 
+// user shall not be entitled to distribute or transfer in any way (including, without, limitation by way of 
+// sublicense) the Assets in any other way than as integrated components of electronic games and interactive media. 
+
+// The above copyright notice and this permission notice must not be removed from any files.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//----------------------------------------------
+using UnityEngine;
+
+namespace FlipWebApps.GameFramework.Scripts.EditorExtras.Editor
+{
+    /// <summary>
+    /// Helper functions for dealing with editor windows, inspectors etc...
+    /// </summary>
+    public class EditorHelper : MonoBehaviour
+    {
+        /// <summary>
+        /// Show a button trimmed to the length of the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="style"></param>
+        /// <returns></returns>
+        public static bool ButtonTrimmed(string text, GUIStyle style)
+        {
+            return GUILayout.Button(text, style, GUILayout.MaxWidth(style.CalcSize(new GUIContent(text)).x));
+        }
+
+
+        /// <summary>
+        /// Show a toggle trimmed to the length of the text
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="text"></param>
+        /// <param name="style"></param>
+        /// <returns></returns>
+        public static bool ToggleTrimmed(bool value, string text, GUIStyle style)
+        {
+            return GUILayout.Toggle(value, text, style, GUILayout.MaxWidth(style.CalcSize(new GUIContent(text)).x));
+        }
+
+
+        /// <summary>
+        /// Show a label trimmed to the length of the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="style"></param>
+        public static void LabelTrimmed(string text, GUIStyle style)
+        {
+            GUILayout.Label(text, style, GUILayout.MaxWidth(style.CalcSize(new GUIContent(text)).x));
+        }
+    }
+}
