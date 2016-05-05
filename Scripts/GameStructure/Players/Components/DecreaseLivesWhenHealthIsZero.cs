04a92d3d1cf51168e80707e48ca8d36cbea49f06
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2016-05-05 18:44:57 +0200

    GameStrcuture: Added DecreaseLivesWhenHealthIsZero component

---
+++ Scripts/GameStructure/Players/Components/DecreaseLivesWhenHealthIsZero.cs
@@ -0,0 +1,51 @@
+﻿//----------------------------------------------
+// Flip Web Apps: Game Framework
+// Copyright © 2016 Flip Web Apps / Mark Hewitt
+//
+// Please direct any bugs/comments/suggestions to http://www.flipwebapps.com
+// 
+// The copyright owner grants to the end user a non-exclusive, worldwide, and perpetual license to this Asset
+// to integrate only as incorporated and embedded components of electronic games and interactive media and 
+// distribute such electronic game and interactive media. End user may modify Assets. End user may otherwise 
+// not reproduce, distribute, sublicense, rent, lease or lend the Assets. It is emphasized that the end 
+// user shall not be entitled to distribute or transfer in any way (including, without, limitation by way of 
+// sublicense) the Assets in any other way than as integrated components of electronic games and interactive media. 
+
+// The above copyright notice and this permission notice must not be removed from any files.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//----------------------------------------------
+
+using FlipWebApps.GameFramework.Scripts.GameStructure.Players.Messages;
+using FlipWebApps.GameFramework.Scripts.Messaging.Components.AbstractClasses;
+using UnityEngine;
+
+namespace FlipWebApps.GameFramework.Scripts.GameStructure.Players.Components
+{
+    /// <summary>
+    /// Decreases the number of lives that the player has when their health reaches zero.
+    /// </summary>
+    [AddComponentMenu("Game Framework/GameStructure/Players/DecreaseLivesWhenHealthIsZero")]
+    [HelpURL("http://www.flipwebapps.com/game-framework/")]
+    public class DecreaseLivesWhenHealthIsZero : RunOnMessage<HealthChangedMessage>
+    {
+        /// <summary>
+        /// Check if the health is zero and if so decrease the number of lives the player has.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public override bool RunMethod(HealthChangedMessage message)
+        {
+            if (message.NewHealth > 0) return true;
+
+            // decrease lives and reset health if they still have lives left.
+            GameManager.Instance.Player.Lives--;
+            if (GameManager.Instance.Player.Lives >= 0)
+                GameManager.Instance.Player.Health = 1;
+            return true;
+        }
+    }
+}
