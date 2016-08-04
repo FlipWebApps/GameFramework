a0c216d06777309a6aca38382a29ea6088aaadb5
unknown <mhew@LT-889943.statoil.net>
2016-08-04 19:34:00 +0200

    Display: GradientBackgroundSplitMiddle component is now implemented and working

--- Scripts/Display/Other/Components/GradientBackgroundSplitMiddle.cs
+++ Scripts/Display/Other/Components/GradientBackgroundSplitMiddle.cs
@@ -24,67 +24,131 @@ using UnityEngine;
 namespace FlipWebApps.GameFramework.Scripts.Display.Other.Components
 {
     /// <summary>
-    /// Description
-    /// Creates a background for the camera, which is a simple gradient blend between two colors.
-    /// Usage
-    /// Attach this script to your camera, and change the top and bottom colors in the inspector as desired. When run, the clear flags for your camera is set to Depth Only, allowing a newly created background camera to show through. A plane with the gradient colors is created, which only the background camera can see. This is done with GradientLayer, which is the only layer that the background camera sees, and your camera is set to ignore. The default, 7, is a built-in (non-user-editable) layer that's not used for anything as of Unity 3.1. It can be changed as necessary.
+    /// Creates a background for the camera, which is a simple gradient blend between three colors.
     /// </summary>
+    /// Attach this script to your camera, and change the top and bottom colors in the inspector as desired. When run, 
+    /// the clear flags for your camera is set to Depth Only, allowing a newly created background camera to show through. 
+    /// A plane with the gradient colors is created, which only the background camera can see. This is done with GradientLayer, 
+    /// which is the only layer that the background camera sees, and your camera is set to ignore. The default, 7, is a 
+    /// built-in (non-user-editable) layer that's not used for anything as of Unity 3.1. It can be changed as necessary.
     [AddComponentMenu("Game Framework/Display/Other/GradientBackgroundSplitMiddle")]
-    [HelpURL("http://www.flipwebapps.com/game-framework/")]
+    [HelpURL("http://www.flipwebapps.com/unity-assets/game-framework/display/")]
     public class GradientBackgroundSplitMiddle : MonoBehaviour
     {
-
+        /// <summary>
+        /// The color to use at the top of the gradient
+        /// </summary>
+        [Tooltip("The color to use at the top of the gradient")]
         public Color TopColor = Color.blue;
+
+        /// <summary>
+        /// The color to use at the middle of the gradient
+        /// </summary>
+        [Tooltip("The color to use at the middle of the gradient")]
         public Color MiddleColor = Color.white;
+
+        /// <summary>
+        /// The color to use at the bottom of the gradient
+        /// </summary>
+        [Tooltip("The color to use at the bottom of the gradient")]
         public Color BottomColor = Color.blue;
+
+        /// <summary>
+        /// The shader that should be used. Select GradientBackgroundShader here
+        /// </summary>
+        [Tooltip("The shader that should be used. Select GradientBackgroundShader here")]
+        public Shader Shader;
+
+        /// <summary>
+        /// The layer on which to generate the gradient
+        /// </summary>
+        [Tooltip("The layer on which to generate the gradient")]
         public int GradientLayer = 7;
-        public float MiddlePosition;
+
+        /// <summary>
+        /// The gradient normalised screen top position
+        /// </summary>
+        [Tooltip("The gradient normalised screen top position")]
+        [Range(0, 1)]
+        public float NormalisedTop = 1;
+
+        /// <summary>
+        /// The gradient normalised screen middle position
+        /// </summary>
+        [Tooltip("The gradient normalised screen middle position")]
+        [Range(0, 1)]
+        public float NormalisedMiddle = 0.5f;
+
+        /// <summary>
+        /// The gradient normalised screen bottom position
+        /// </summary>
+        [Tooltip("The gradient normalised screen bottom position")]
+        [Range(0, 1)]
+        public float NormalisedBottom = 0;
 
         void Awake()
         {
-            Debug.LogError("Script needs updating");
-            /*
+            var attachedCamera = gameObject.GetComponent<Camera>();
             GradientLayer = Mathf.Clamp(GradientLayer, 0, 31);
-            if (!GetComponent<Camera>())
+            if (!attachedCamera)
             {
-                Debug.LogError("Must attach GradientBackground script to the camera");
+                Debug.LogError("You must attach a GradientBackground script to the same gameobject as a Camera");
                 return;
             }
 
-            GetComponent<Camera>().clearFlags = CameraClearFlags.Depth;
-            GetComponent<Camera>().cullingMask = GetComponent<Camera>().cullingMask & ~(1 << GradientLayer);
-            Camera gradientCam = new GameObject("Gradient Cam", typeof(Camera)).GetComponent<Camera>();
-            gradientCam.depth = GetComponent<Camera>().depth - 1;
+            attachedCamera.clearFlags = CameraClearFlags.Depth;
+            attachedCamera.cullingMask = attachedCamera.cullingMask & ~(1 << GradientLayer);
+            var gradientCam = new GameObject("Gradient Cam", typeof(Camera)).GetComponent<Camera>();
+            gradientCam.depth = attachedCamera.depth - 1;
             gradientCam.cullingMask = 1 << GradientLayer;
 
-            Mesh mesh = new Mesh();
-            mesh.vertices = new Vector3[4] { new Vector3(-100f, .577f, 1f), new Vector3(100f, .577f, 1f), new Vector3(-100f, middlePosition, 1f), new Vector3(100f, middlePosition, 1f) };
+            //1.154 is the height of a plane at z=0 with the default camera field of view of 60. .577 is half this.
+            var top = -.577f + (NormalisedTop * 1.154f);
+            var bottom = -.577f + (NormalisedBottom * 1.154f);
+            var middle = -.577f + (NormalisedMiddle * 1.154f);
 
-            mesh.colors = new Color[4] { topColor, topColor, middleColor, middleColor };
+            // create top plane
+            var mesh = new Mesh
+            {
+                vertices =
+                    new[]
+                    {
+                        new Vector3(-100f, top, 1f), new Vector3(100f, top, 1f), new Vector3(-100f, middle, 1f),
+                        new Vector3(100f, middle, 1f)
+                    },
+                colors = new[] { TopColor, TopColor, MiddleColor, MiddleColor },
+                triangles = new[] { 0, 1, 2, 1, 3, 2 }
+            };
 
-            mesh.triangles = new int[6] { 0, 1, 2, 1, 3, 2 };
-
-            Material mat = new Material("Shader \"Vertex Color Only\"{Subshader{BindChannels{Bind \"vertex\", vertex Bind \"color\", color}Pass{}}}");
-            GameObject gradientPlane = new GameObject("Gradient Plane", typeof(MeshFilter), typeof(MeshRenderer));
-
-            ((MeshFilter)gradientPlane.GetComponent(typeof(MeshFilter))).mesh = mesh;
-            gradientPlane.GetComponent<Renderer>().material = mat;
-            gradientPlane.layer = GradientLayer;
-
-            // create bottom gradient as reverse of top
-            mesh = new Mesh();
-            mesh.vertices = new Vector3[4] { new Vector3(-100f, middlePosition, 1f), new Vector3(100f, middlePosition, 1f), new Vector3(-100f, -.577f, 1f), new Vector3(100f, -.577f, 1f) };
-
-            mesh.colors = new Color[4] { middleColor, middleColor, bottomColor, bottomColor };
-
-            mesh.triangles = new int[6] { 0, 1, 2, 1, 3, 2 };
-
-            gradientPlane = new GameObject("Gradient Plane Bottom", typeof(MeshFilter), typeof(MeshRenderer));
+            var mat = new Material(Shader);
+            var gradientPlane = new GameObject("Gradient Plane", typeof(MeshFilter), typeof(MeshRenderer));
 
             ((MeshFilter)gradientPlane.GetComponent(typeof(MeshFilter))).mesh = mesh;
-            gradientPlane.GetComponent<Renderer>().material = mat;
+            var gradRend = gradientPlane.GetComponent<Renderer>();
+            gradRend.material = mat;
             gradientPlane.layer = GradientLayer;
-        */
+
+            // create bottom plane as an inverse copy of the top
+            mesh = new Mesh
+            {
+                vertices =
+                    new[]
+                    {
+                        new Vector3(-100f, middle, 1f), new Vector3(100f, middle, 1f), new Vector3(-100f, bottom, 1f),
+                        new Vector3(100f, bottom, 1f)
+                    },
+                colors = new[] { MiddleColor, MiddleColor, BottomColor, BottomColor },
+                triangles = new[] { 0, 1, 2, 1, 3, 2 }
+            };
+
+            mat = new Material(Shader);
+            gradientPlane = new GameObject("Gradient Plane", typeof(MeshFilter), typeof(MeshRenderer));
+
+            ((MeshFilter)gradientPlane.GetComponent(typeof(MeshFilter))).mesh = mesh;
+            gradRend = gradientPlane.GetComponent<Renderer>();
+            gradRend.material = mat;
+            gradientPlane.layer = GradientLayer;
+
         }
 
     }
