b38e3c6e2b89d1f6cb9fb6ca2d718bb8c195416b
unknown <MHEW@LT-889943.statoil.net>
2016-09-23 07:40:30 +0200

    Refactor of GameItemExtensionInspector for inheritance

--- Scripts/GameStructure/Editor/GameItemExtensionInspector.cs
+++ Scripts/GameStructure/Editor/GameItemExtensionInspector.cs
@@ -1,8 +1,26 @@
-﻿using FlipWebApps.GameFramework.Scripts.EditorExtras.Editor;
+﻿//----------------------------------------------
+// Flip Web Apps: Game Framework
+// Copyright © 2016 Flip Web Apps / Mark Hewitt
+//
+// Please direct any bugs/comments/suggestions to http://www.flipwebapps.com
+// 
+// The copyright owner grants to the end user a non-exclusive, worldwide, and perpetual license to this Asset
+// to integrate only as incorporated and embedded components of electronic games and interactive media and 
+// distribute such electronic game and interactive media. End user may modify Assets. End user may otherwise 
+// not reproduce, distribute, sublicense, rent, lease or lend the Assets. It is emphasized that the end 
+// user shall not be entitled to distribute or transfer in any way (including, without, limitation by way of 
+// sublicense) the Assets in any other way than as integrated components of electronic games and interactive media. 
+
+// The above copyright notice and this permission notice must not be removed from any files.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//----------------------------------------------
+
 using FlipWebApps.GameFramework.Scripts.GameStructure.GameItems.ObjectModel;
-using FlipWebApps.GameFramework.Scripts.Preferences;
 using UnityEditor;
-using UnityEngine;
 
 namespace FlipWebApps.GameFramework.Scripts.GameStructure.Editor
 {
@@ -13,7 +31,8 @@ namespace FlipWebApps.GameFramework.Scripts.GameStructure.Editor
         SerializedProperty _giNameProperty;
         SerializedProperty _giDescriptionProperty;
         SerializedProperty _giValueToUnlockProperty;
-        void OnEnable()
+
+        protected virtual void OnEnable()
         {
             _gameItemExtension = (GameItemExtension)target;
             // get serialized objects so we can use attached property drawers (e.g. tooltips, ...)
@@ -27,21 +46,33 @@ namespace FlipWebApps.GameFramework.Scripts.GameStructure.Editor
             //DrawDefaultInspector();
             serializedObject.Update();
 
-            DrawGameItemExtension();
+            DrawGUI();
             serializedObject.ApplyModifiedProperties();
         }
 
-        public void DrawGameItemExtension()
+        protected virtual void DrawGUI()
         {
             EditorGUILayout.LabelField("Game Item Extension", EditorStyles.boldLabel);
+            EditorGUILayout.HelpBox("Use these settings to provide additional customisation for GameItems.\n\nThere are also Extensions for specific GameItems such as Level and Player. In addition you can create your own derived classes to hold custom properties and / or code", MessageType.Info);
             // Game Item setup
             EditorGUILayout.BeginVertical("Box");
-                EditorGUI.indentLevel += 1;
-                    EditorGUILayout.PropertyField(_giNameProperty, new GUIContent("Name"));
-                    EditorGUILayout.PropertyField(_giDescriptionProperty, new GUIContent("Description"));
-                    EditorGUILayout.PropertyField(_giValueToUnlockProperty, new GUIContent("Value to Unlock"));
-                EditorGUI.indentLevel -= 1;
+            DrawBasicProperties();
             EditorGUILayout.EndVertical();
+        }
+
+        protected void DrawBasicProperties()
+        {
+            EditorGUILayout.LabelField("Basic Properties", EditorStyles.boldLabel);
+            EditorGUI.indentLevel += 1;
+            EditorGUILayout.PropertyField(_giNameProperty);
+            EditorGUILayout.PropertyField(_giDescriptionProperty);
+            _gameItemExtension.OverrideValueToUnlock = EditorGUILayout.BeginToggleGroup("Override ValueToUnlock", _gameItemExtension.OverrideValueToUnlock);
+            // TODO: Have a int field that allows an empty value that signifies don't override.
+            //var valueToUnlock = EditorGUILayout.TextField("ValueToUnlock", _gameItemExtension.OverrideValueToUnlock ? _gameItemExtension.ValueToUnlock.ToString() : "");
+            //valueToUnlock.S
+            EditorGUILayout.PropertyField(_giValueToUnlockProperty);
+            EditorGUILayout.EndToggleGroup();
+            EditorGUI.indentLevel -= 1;
         }
     }
 }
