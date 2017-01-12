a9467f2332e95eafbfe7026653863eaaa39506d0
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2017-01-12 21:42:02 +0100

    added GenericGameItemEditor + other related editor cleanup

--- Scripts/GameStructure/Editor/GenericGameItemEditor.cs
+++ Scripts/GameStructure/Editor/GenericGameItemEditor.cs
@@ -19,19 +19,18 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //----------------------------------------------
 
-using FlipWebApps.GameFramework.Scripts.GameStructure.Players.ObjectModel;
+using FlipWebApps.GameFramework.Scripts.GameStructure.Worlds.ObjectModel;
 using UnityEditor;
-using UnityEngine;
 
 namespace FlipWebApps.GameFramework.Scripts.GameStructure.Editor
 {
-    [CustomEditor(typeof(Player))]
-    public class PlayerEditor : GameItemEditor
+    [CustomEditor(typeof(GenericGameItem))]
+    public class GenericGameItemEditor : GameItemEditor
     {
         protected override void DrawGUI()
         {
-            EditorGUILayout.LabelField("Plsyer Extension", EditorStyles.boldLabel);
-            EditorGUILayout.HelpBox("Use these settings to provide customisation for Players.\n\nFor automatic loading instances should be in a folder 'Resources\\Player' and named 'Player_<number>'\n\nYou can create your own Player derived classes to hold custom properties and / or code", MessageType.Info);
+            EditorGUILayout.LabelField("Generic GameItem Extension", EditorStyles.boldLabel);
+            EditorGUILayout.HelpBox("Use these settings to provide customisation for GenericGameItems.\n\nFor automatic loading instances should be in a folder 'Resources\\GenericGameItem' and named 'GenericGameItem_<number>'\n\nYou can create your own GenericGameItem derived classes to hold custom properties and / or code", MessageType.Info);
             DrawProperties();
         }
     }
--- Scripts/GameStructure/Editor/WorldEditor.cs
+++ Scripts/GameStructure/Editor/WorldEditor.cs
@@ -31,9 +31,7 @@ namespace FlipWebApps.GameFramework.Scripts.GameStructure.Editor
         {
             EditorGUILayout.LabelField("World Extension", EditorStyles.boldLabel);
             EditorGUILayout.HelpBox("Use these settings to provide customisation for Worlds.\n\nFor automatic loading instances should be in a folder 'Resources\\World' and named 'World_<number>'\n\nYou can create your own World derived classes to hold custom properties and / or code", MessageType.Info);
-            EditorGUILayout.BeginVertical("Box");
             DrawProperties();
-            EditorGUILayout.EndVertical();
         }
     }
 }
