6520dbede231f5dd94854a93895cc1618f28b95e
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2017-03-14 22:19:59 +0100

    GameItemButton references moved from CurrentItem (deprecated) to GameItem

--- Scripts/GameStructure/GameItems/Components/AbstractClasses/GameItemButton.cs
+++ Scripts/GameStructure/GameItems/Components/AbstractClasses/GameItemButton.cs
@@ -542,12 +542,12 @@ namespace GameFramework.GameStructure.GameItems.Components.AbstractClasses
         {
             var isUnlockedAndAnimationShown = GameItem.IsUnlocked && GameItem.IsUnlockedAnimationShown;
 
-            UIHelper.SetTextOnChildGameObject(gameObject, "Name", CurrentItem.Name, true);
+            UIHelper.SetTextOnChildGameObject(gameObject, "Name", GameItem.Name, true);
 
             if (DisplayImage != null)
             {
-                var selectionMenuSprite = CurrentItem.GetSpriteByType(GameItem.LocalisableSpriteType.SelectionMenu);
-                DisplayImage.sprite = selectionMenuSprite ?? CurrentItem.Sprite;
+                var selectionMenuSprite = GameItem.GetSpriteByType(GameItem.LocalisableSpriteType.SelectionMenu);
+                DisplayImage.sprite = selectionMenuSprite ?? GameItem.Sprite;
             }
 
             if (LockGameObject != null)
@@ -563,7 +563,7 @@ namespace GameFramework.GameStructure.GameItems.Components.AbstractClasses
             {
                 ValueToUnlockGameObject.SetActive(GameItem.UnlockWithCoins && !isUnlockedAndAnimationShown);
                 if (ValueToUnlockAmount != null)
-                    ValueToUnlockAmount.text = "x" + CurrentItem.ValueToUnlock.ToString();
+                    ValueToUnlockAmount.text = "x" + GameItem.ValueToUnlock.ToString();
             }
 
             if (SelectionMode == GameItemButton.SelectionModeType.Select && HighlightGameObject != null)
