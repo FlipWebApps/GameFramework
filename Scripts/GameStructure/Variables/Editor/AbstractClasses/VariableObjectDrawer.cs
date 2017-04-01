e1f009aacce90e943c1d8118b6abd36da5ae3690
Mark Hewitt <mark_a_hewitt@yahoo.co.uk>
2017-04-01 10:46:15 +0200

    removed unused using

--- Scripts/GameStructure/Variables/Editor/AbstractClasses/VariableObjectDrawer.cs
+++ Scripts/GameStructure/Variables/Editor/AbstractClasses/VariableObjectDrawer.cs
@@ -19,7 +19,6 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //----------------------------------------------
 
-using System;
 using GameFramework.GameStructure.Variables.ObjectModel;
 using UnityEditor;
 using UnityEngine;
